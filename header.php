8adc9e5fb824adc5aa53ed6f176f33b17fb20f5b
Ivana <ivana@agenciar8.com.br>
2022-12-22 15:02:32 -0300

    fisrt commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta name="author" content="Agência R8 | Marketing Digital - www.agenciar8.com.br">
+  <meta name="description" content="<?php bloginfo('description'); ?>">
+
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
+
+  <title><?php bloginfo('name'); wp_title();?></title>
+</head>
+<body>
+  
+</body>
+</html>
